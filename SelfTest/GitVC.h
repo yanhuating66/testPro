db9813a4215e70f71b2e54f985d42690262de414
严华停 <appleplus@yanhuatingdeMac-mini.local>
2020-06-10 10:33:52 +0800

    添加了新的控制器

---
+++ SelfTest/GitVC.h
@@ -0,0 +1,17 @@
+//
+//  GitVC.h
+//  SelfTest
+//
+//  Created by 严华停 on 2020/6/10.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GitVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SelfTest/GitVC.m
@@ -0,0 +1,33 @@
+//
+//  GitVC.m
+//  SelfTest
+//
+//  Created by 严华停 on 2020/6/10.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import "GitVC.h"
+
+@interface GitVC ()
+
+@end
+
+@implementation GitVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor =[UIColor greenColor];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
